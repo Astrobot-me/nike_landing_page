0b914d7a68681d9317c7b77af879b241127879ff
astrobot-me <autobot268@gmail.com>
2024-06-22 22:01:53 +0530

    deployment commit 3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"nike_landing_page",
   build: {
     outDir: 'dist'
   }
